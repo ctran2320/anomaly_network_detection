32c801aef8dfa07f11474a1d44ffac5895c2905f
jjharsono1 <jjharson@ucsd.edu>
2022-02-05 15:08:04 -0800

    eda

--- src/eda.py
+++ src/eda.py
@@ -1,1 +1,80 @@
 # For figures that are used for EDA
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+from os import listdir, join
+import seaborn as sns
+import numpy as np
+import sys
+
+from etl import clean_df
+
+"""
+Creates a correlation matrix for the features
+"""
+def plot_correlation(data_filename, outdir, filename="correlation_matrix.png"):
+    raise NotImplementedError()
+
+"""
+Creates a time plot of the number of packets being sent per second and where packets are dropped if file exists
+
+conditions data was generated on, format: f"{latency1} {loss1} {latency2} {loss2} {random drops}"
+data_path: path with folder of loss logs and packet data
+outdir: directry to put the graph
+filename: name of the graph image to save
+"""
+def plot_timeseries(conditions, data_path, outdir, filename="timeseries.png"):
+    # list the packet data files
+    raw_data_path = join(data_path, 'packet_data')
+    raw_files_loss = [join(raw_data_path, f) for f in listdir(raw_data_path)]
+
+    # packet drop data
+    raw_data_path = join(data_path, 'loss_logs')
+    raw_files = [join(raw_data_path, f) for f in listdir(raw_data_path)]
+
+    # associates conditions with filenames
+    metrics = []
+    filenames = {}
+    for file in raw_files:
+        labels = file.split('_')[-1].split('-')
+        print(labels)
+        metrics = metrics + [labels[0] +' '+ labels[1] +' '+ labels[3]+' '+labels[4]+' '+labels[2]]
+        filenames[labels[0] +' '+ labels[1] +' '+ labels[3]+' '+labels[4]+' '+labels[2]] = [file]
+        
+    for file in raw_files_loss:
+        labels = file.split('.')[-2].split('-')
+        filenames[labels[1] +' '+ labels[2] +' '+ labels[4]+' '+labels[5]+' '+labels[3]] += [file]
+
+    # find the data that meets the conditions
+    f = filenames[conditions][0]
+    f_drop = filenames[conditions][1]
+
+    data = pd.read_csv(f)
+    data_drop = pd.read_csv(f_drop, header=None)
+    data_drop.columns = ['drop', 'Time', 'IP1', 'Port1', 'IP2', 'Port2', '?']
+    clean = clean_df(data)
+    plt.plot(clean['1->2Pkts'])
+
+    port = data_drop['Port1']
+    time = data_drop['Time'] - data['Time'].min()
+    max_pkts = clean['1->2Pkts'].max()
+
+    for i in range(len(time)):
+        # confirmation (computer 2)
+        if port[i] == 5001:
+            plt.vlines(time[i], 0, max_pkts, colors='C1', alpha=0.3)
+            pass
+        # data (computer 1)
+        else:
+            plt.vlines(time[i], 0, max_pkts, colors='C2', alpha=0.3)
+            pass
+
+    plt.vlines(180, 0, max_pkts, colors='C3')
+
+    plt.title(f'packets per second, conditions: {conditions}')
+    plt.ylabel('packets per second')
+    plt.xlabel('time')
+
+    # plt.legend()
+    # saves graph to directory
+    plt.savefig(os.path.join(outdir, filename))
