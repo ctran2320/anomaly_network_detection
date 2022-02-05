e21e2bc68aacc846749d4d580233303ca10ea349
Charlie Tran <cht104@ucsd.edu>
2022-02-05 14:43:14 -0800

    updates to etl and metrics

--- src/metrics.py
+++ src/metrics.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib as plt
 import numpy as np
 
+## plot_results show time series graph of network traffic, predictions, and flagged anomalies on original scale or log scale
 def plot_results(preds, data,anomalies, n, log=False):
     preds = pd.DataFrame(preds)
     preds['time'] = np.arange(n,len(preds)+n)
@@ -10,10 +11,12 @@ def plot_results(preds, data,anomalies, n, log=False):
     preds['anomaly'] = anomalies
     preds['actual'] =data.total_pkts[n:]
     anom = preds[preds.anomaly==1]
+    # Returns plot on log scale
     if log == True:
         plt.plot(np.log(data.total_pkts),label='actual')
         plt.plot(preds.preds,label='preds')
         plt.scatter(anom.index,np.log(anom.actual),label='anomaly',c='r',s=100)
+    # Returns plot on normal scale
     if log == False:
         plt.plot(data.total_pkts,label='actual')
         plt.scatter(anom.index,anom.actual,label='anomaly',c='r',s=100)
@@ -21,4 +24,5 @@ def plot_results(preds, data,anomalies, n, log=False):
     plt.xlabel('Time (10s)')
     plt.ylabel('Total Packets Sent')
     plt.title('Anomaly Detection 40-5000-320-1250 (99% CI)')
+    # Outputs and saves graph image
     plt.savefig('output.png', dpi=300)
